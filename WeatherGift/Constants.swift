86376db16b68fdde4cc44a8b0180a14d321d14c2
Alberto Medina <albertomedina@macbook-pro-9.local>
2019-03-18 14:50:53 -0400

    7.15 Complete

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Alberto Medina on 3/18/19.
+//  Copyright © 2019 Alberto Medina. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "38512a7d6e4f686d542c741982a4a710/"
